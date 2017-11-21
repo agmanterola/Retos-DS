b16935a743072c06c5f255714dcb280715f207ec
Aitor Garcia <aitor.garcia@bluetab.net>
2017-11-21 09:16:35 +0100

    Correcciones comentadas sobre el ejercicio 2 (crímenes) del reto 2

--- 02_consulta_apis/02_crimenes.R
+++ 02_consulta_apis/02_crimenes.R
@@ -24,10 +24,9 @@ library(httr)
 
 # Creo la URL
 direccion <- "http://nominatim.openstreetmap.org/reverse?format=jsonv2"
-latitud <- "&lat=-34.44076"
-longitud <- "&lon=-58.70521"
-latitud <- "&lat=52.643950"
-longitud <- "&lon=-1.143042"
+latitud <- "&lat=51.4965946"
+longitud <- "&lon=-0.1436476"
+
 # Defino la URL uniendo los parámetros y quitando espacios entre ellos
 url_lugar <- paste(direccion,latitud,longitud,sep="")
 url_lugar
@@ -38,18 +37,17 @@ info_lugar <- content(peticion_lugar)
 
 # Imprimo cosas sobre el lugar obtenido
 lugar <- info_lugar$address
-print(paste("La dirección del lugar es Calle ",lugar$road," nº ",lugar$house_number,
-            " de la ciudad de ",lugar$city," C.P.:",lugar$postcode,", ",lugar$state, sep=""))
+#print(paste("La dirección del lugar es Calle ",lugar$road," nº ",lugar$house_number,
+#" de la ciudad de ",lugar$city," C.P.:",lugar$postcode,", ",lugar$state, sep=""))
+
 
 # Compruebo información de crímenes por la zona:
-direccion <- "https://data.police.uk/api/crimes-street/all-crime?"
-fecha_crimen <-"&date=2017-04"
-
-# Con los datos que tengo, preparo la consulta para los crímenes
-url_crimen <- paste(direccion,"lat=",info_lugar$lat,"&lng=",info_lugar$lon,fecha_crimen, sep="")
+#direccion <- "https://data.police.uk/api/crimes-street/all-crime?"
+direccion <- "https://data.police.uk/api/crimes-at-location?"
+fecha_crimen <-"date=2017-04"
 
 # Consulto el lugar por coordenada y fecha
-peticion_crimen <- GET(url_crimen)
+peticion_crimen<- GET(direccion, query = list(dat = "2017-04", lat = 51.4965946, lng = -0.1436476))
 contenido <- content(peticion_crimen)
 
 # Selecciono las categorías de crímenes de la zona
